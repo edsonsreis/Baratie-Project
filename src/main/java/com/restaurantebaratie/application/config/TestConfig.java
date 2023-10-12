65c25a9d024ac5e66ea5c60270b71af5bbccbafa
Edson Reis <edsomreis@gmail.com>
2023-10-12 17:42:02 -0300

    Product

--- src/main/java/com/restaurantebaratie/application/config/TestConfig.java
+++ src/main/java/com/restaurantebaratie/application/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.restaurantebaratie.application.entities.Category;
 import com.restaurantebaratie.application.entities.Order;
+import com.restaurantebaratie.application.entities.Product;
 import com.restaurantebaratie.application.entities.User;
 import com.restaurantebaratie.application.entities.enums.OrderStatus;
 import com.restaurantebaratie.application.repositories.CategoryRepository;
 import com.restaurantebaratie.application.repositories.OrderRepository;
+import com.restaurantebaratie.application.repositories.ProductRepository;
 import com.restaurantebaratie.application.repositories.UserRepository;
 
 @Configuration
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -36,7 +41,16 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers"); 
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
